d0fcb5e9153a14d5ac73176826a75387c7be9bf3
Szymon Walter <walter.szymon.98@gmail.com>
2017-07-29 10:27:04 +0200

    Add water

---
+++ src/cell/water.lua
@@ -0,0 +1,36 @@
+
+local cpml = require('cpml')
+local cell = require('src/cell')
+local water = {}
+local mt = {__index = water}
+
+local global
+
+function water.new(x, y)
+    if not global then
+        global = lib4.root.children.global
+    end
+
+    local self = cell.new()
+    setmetatable(self, mt)
+
+    self.position = cpml.vec2(x, y)
+
+    self.type = 'water'
+
+    self.character = 'c'
+    self.colour = {49, 49, 71}
+
+    self.radius = 5
+    self.stats = {}
+    self.provides = {food = 1, water = 4}
+    self.requires = {}
+
+    return self
+end
+
+setmetatable(water, {
+    __index = cell,
+    __call = function(_, ...) return water.new(...) end,
+})
+return water
